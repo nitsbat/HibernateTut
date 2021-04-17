9c94045086193f1a9a8859df3cc1c58955ce3b81
Nitin Bisht <3498georgian@gmail.com>
2021-04-17 16:13:13 +0530

    change pom and hbm2ddl configurations and also some annotations to change the table or column name.

--- src/main/java/org/bisht/model/UserDetails.java
+++ src/main/java/org/bisht/model/UserDetails.java
@@ -1,13 +1,17 @@
 package org.bisht.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "User")
 public class UserDetails {
 
     @Id
+    @Column(name = "id")
     private int userId;
+
+    @Column(name = "name")
     private String username;
 
     public int getUserId() {
